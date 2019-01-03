34985277a3fb51213dee36d6af68a02a871f4c26
Natem <hugo.niort9@gmail.com>
2019-01-03 15:44:28 +0100

    cart page dynamic

---
+++ application/controllers/Cart.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hugo
+ * Date: 29/12/2018
+ * Time: 21:45
+ */
+
+class Cart
+{
+
+}
---
+++ application/views/cart/order-summary.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hugo
+ * Date: 29/12/2018
+ * Time: 21:40
+ */
